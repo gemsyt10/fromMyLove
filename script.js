f09dfc79eff5b7e5771f4008e23233cced52eea9
Gemsyt10 <gemsyt10@gmail.com>
2026-02-20 22:06:32 +0200

    Update timer text and reset heart image

--- script.js
+++ script.js
@@ -7,4 +7,5 @@ const letterFromMyLove = document.querySelector(".letter");
 const letterActivateBtn = document.querySelector(".letter-btn")
 const thisDate = new Date();
 const date = new Date(2026, 0, 25, 12, 0, 0);
-
+TIMER.textContent = "Відносини розірвано..."
+heart.src = ""
